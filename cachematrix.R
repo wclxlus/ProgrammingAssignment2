a3a5c22f42f17ed543d9a8b16ebf65b4b2302f1b
wclxlus <wclxl@hotmail.com>
2014-09-28 00:38:32 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Programming Assignment 2: Lexical Scoping
+# reference: https://class.coursera.org/rprog-007/human_grading/view/courses/972580/assessments/3/submissions
 
-## Write a short comment describing this function
+# run the test codes 
+# > m<-makeCacheMatrix()
+# > m$set(matrix(c(4,2,7,6), 2,2))
+# > cacheSolve(m)
+# calculating matrix inverse cached data
+# [,1] [,2]
+# [1,]  0.6 -0.7
+# [2,] -0.2  0.4
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached matrix inverse")
+        return(i)
+    }
+    data <- x$get()
+    message("calculating matrix inverse")
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
